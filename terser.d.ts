d3051df34cc2824c6accfea40cc0119736240747
roncli <roncli@roncli.com>
2026-03-02 21:51:54 -0800

    Use correct tsconfig.json settings and remove cjs workaround.

--- terser.d.ts
+++
@@ -1,7 +0,0 @@
-// Minimal CommonJS-compatible types for terser.
-declare module "terser" {
-    export function minify(
-        files: string | {[file: string]: string},
-        options?: any
-    ): Promise<{code: string}>
-}
